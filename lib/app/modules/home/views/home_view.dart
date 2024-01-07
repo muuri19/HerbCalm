b66a07e6ea8ad9b23ba488d769cbbb59c080e330
MuhammadZulfikarIkhsan8 <zulmuh815@gmail.com>
2024-01-07 20:58:04 +0700

    Update home view

--- lib/app/modules/home/views/home_view.dart
+++ lib/app/modules/home/views/home_view.dart
@@ -15,7 +15,7 @@ class HomeView extends GetView<HomeController> {
       ),
       body: const Center(
         child: Text(
-          'HomeView is working',
+          'HomeView is workingg',
           style: TextStyle(fontSize: 20),
         ),
       ),
